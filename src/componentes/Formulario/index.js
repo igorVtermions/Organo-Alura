687329f9f87ffacd326b497de33216f840955543
Igor Vinicius <igorviniciusf10@gmail.com>
2023-07-10 21:17:41 -0300

    recive value input's

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Botao from '../Botao/index'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa/index.js'
@@ -14,20 +15,28 @@ const Formulario = () => {
         'mobile',
         'inovação'
     ]
+    const [name, setName] = useState('')
+    const [post, setPost] = useState('')
+    const [img, setImg] = useState('')
+    const [team, setTeam] = useState('')
+
 
     const save = (event)=>{
+       
         event.preventDefault()
 
-        console.log('submit!!')
+        console.log(`Novo Card: ${name} ${post} ${img} ${team}`)
     }
+
+
     return (
         <section className="formulario">
             <form onSubmit={save}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto required={true} label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto required={true} label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <CampoTexto required={true} label="Nome" placeholder="Digite seu nome" valor={name} altered={valor => setName(valor)}/>
+                <CampoTexto required={true} label="Cargo" placeholder="Digite seu cargo" valor={post} altered = {valor => setPost(valor)} />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" valor={img} altered={valor => setImg(valor)} />
+                <ListaSuspensa label="Time" itens={times} valor={team} altered={valor => setTeam(valor)}/>
                 <Botao>
                     <p>Criar Card</p>
                 </Botao>
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -8,7 +8,7 @@ export default function ListaSuspensa(props){
     return(
         <div className='campo-time'>
             <label>{props.label}</label>
-            <select>
+            <select onChange={event => props.altered(event.target.value)}>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
                 })}
